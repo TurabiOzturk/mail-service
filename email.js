404881efc60c256dc2c1ab74b3e4027411dc3e4d
Turabi OZTURK <154705776+TurabiOzturk@users.noreply.github.com>
2024-07-14 03:06:19 +0300

    fixing email.js

--- email.js
+++ email.js
@@ -71,15 +71,67 @@ class GetEmail {
   }
 
   async getEmailDetails() {
-    this.currentEmail = this.fetchedMails.filter((email) =>
-        email.mailId == this.currentEmailId
-
-      );
-      console.log(this.currentEmail);
+    this.currentEmail = this.fetchedMails.filter(
+      (email) => email.mailId == this.currentEmailId
+    );
+    console.log(this.currentEmail);
   }
+
+  async parseEmail() {
+    //const selectedFolderTab = document.getElementById(`"${this.currentFolder.toLowerCase()}"`);
+    const mailRow = document.getElementById("mail-row");
+
+    const mailTable = document.createElement("table");
+    mailTable.classList.add("inbox-table");
+    const mailTableBody = document.createElement("tbody");
+    mailTable.appendChild(mailTableBody);
+
+     // suck ass date conversion start
+
+     const timestamp = this.currentEmail[0].timestamp;
+     const date = new Date(timestamp);
+     const options = {  day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false // Use 12-hour clock (AM/PM)  
+        };
+     const formattedDate = date.toLocaleDateString("en-UK", options);
+     console.log(this.currentFolder);
+     // suck ass date conversion end
+
+    this.fetchedMails.length > 0
+      ? mailRow.appendChild(mailTable)
+      : (mailRow.innerHTML = "no emails here!");
+
+      mailTableBody.innerHTML += `
+      <td style="font-size: 40px">
+        
+          ${this.currentEmail[0].subject}</td>
+      <tr>
+      <td>${this.currentEmail[0].isStarred
+          ? '<i class="bi bi-star-fill"></i>'
+          : '<i class="bi bi-star"></i>'
+      }</td>
+      <td>
+        
+          ${this.currentEmail[0].from}</td>
+      
+      <td style="overflow: hidden; text-overflow: ellipsis;">
+
+      ${this.currentEmail[0].body}
+      </td>
+      <td>${formattedDate}</td>
+      </tr>
+   `;
+}
+
+  
 }
 
 const email = new GetEmail();
 await email.loadEmail();
 await email.fetchFolder();
 await email.getEmailDetails();
+await email.parseEmail();
